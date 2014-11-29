57d8bb7cbc058e6f47538667dfe420c4582f9f58
“digitallawyer” <pieter@lawgives.com>
2014-11-28 19:34:26 -0800

    complete content and page structure

--- app/controllers/declaration_controller.rb
+++ app/controllers/declaration_controller.rb
@@ -13,4 +13,85 @@ class DeclarationController < ApplicationController
 
   def a3
   end
+
+  def a4
+  end
+
+  def a5
+  end
+
+  def a6
+  end
+
+  def a7
+  end
+
+  def a8
+  end
+
+  def a9
+  end
+
+  def a10
+  end
+
+  def a11
+  end
+
+  def a12
+  end
+
+  def a13
+  end
+
+  def a14
+  end
+
+  def a15
+  end
+
+  def a16
+  end
+
+  def a17
+  end
+
+  def a18
+  end
+
+  def a19
+  end
+
+  def a20
+  end
+
+  def a21
+  end
+
+  def a22
+  end
+
+  def a23
+  end
+
+  def a24
+  end
+
+  def a25
+  end
+
+  def a26
+  end
+
+  def a27
+  end
+
+  def a28
+  end
+
+  def a29
+  end
+
+  def a30
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+def lg_current_page(controller: nil, action: nil, show_string: '', use_these_params: nil)
+    use_these_params ||= params
+    return ''.freeze if controller && (controller.is_a?(Array) ? controller.map(&:to_s).exclude?(use_these_params[:controller].to_s) : use_these_params[:controller].to_s != controller.to_s)
+    return ''.freeze if action && (action.is_a?(Array) ? action.map(&:to_s).exclude?(use_these_params[:action].to_s) : use_these_params[:action].to_s != action.to_s)
+    show_string
+  end
+
 end
